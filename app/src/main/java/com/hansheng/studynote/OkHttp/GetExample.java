d232ee9d07e89bcecc3ad47dff293f49d85967c6
fuqin <shenghanming@gmail.com>
2016-07-02 10:58:19 +0800

    更新OkHttp用例

---
+++ app/src/main/java/com/hansheng/studynote/OkHttp/GetExample.java
@@ -0,0 +1,34 @@
+package com.hansheng.studynote.OkHttp;
+
+
+
+import android.annotation.TargetApi;
+import android.os.Build;
+
+import java.io.IOException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+/**
+ * Created by hansheng on 2016/7/2.
+ */
+public class GetExample {
+
+    OkHttpClient client=new OkHttpClient();
+    @TargetApi(Build.VERSION_CODES.KITKAT)
+    String run(String url) throws IOException {
+        Request request=new Request.Builder()
+                .url(url)
+                .build();
+       Response response=client.newCall(request).execute();
+            return response.body().toString();
+
+    }
+    public static void main(String... args) throws IOException {
+        GetExample example=new GetExample();
+        String response=example.run("https://raw.github.com/square/okhttp/master/README.md");
+        System.out.println(response);
+    }
+}
---
+++ app/src/main/java/com/hansheng/studynote/OkHttp/recipes/AsynchronousGet.java
@@ -0,0 +1,39 @@
+package com.hansheng.studynote.OkHttp.recipes;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Headers;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import okhttp3.internal.framed.Header;
+
+/**
+ * Created by hansheng on 2016/7/2.
+ */
+public class AsynchronousGet {
+
+    private static final OkHttpClient client=new OkHttpClient();
+    public static void run(){
+        final Request request=new Request.Builder()
+                .url("http://publicobject.com/helloworld.txt")
+                .build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                RequestBody requestBody=request.body();
+                if(!response.isSuccessful())throw new IOException("Unexpected code " + response);
+                Headers responseHeaders=response.headers();
+
+            }
+        });
+    }
+}
