06484699c946b5ebe8d40c70951671ab5078a272
fuqin <shenghanming@gmail.com>
2016-07-18 22:43:23 +0800

    add NestListView

---
+++ app/src/main/java/com/hansheng/studynote/crawler/Controller.java
@@ -0,0 +1,49 @@
+package com.hansheng.studynote.crawler;
+
+
+import edu.uci.ics.crawler4j.crawler.CrawlConfig;
+import edu.uci.ics.crawler4j.crawler.CrawlController;
+import edu.uci.ics.crawler4j.fetcher.PageFetcher;
+import edu.uci.ics.crawler4j.robotstxt.RobotstxtConfig;
+import edu.uci.ics.crawler4j.robotstxt.RobotstxtServer;
+
+/**
+ * Created by hansheng on 2016/7/18.
+ */
+public class Controller {
+    public static void main(String... args)  {
+        String crawlStorageFolder = "/data/crawl/root";
+        int numberOfCrawlers = 7;
+
+        CrawlConfig config = new CrawlConfig();
+        config.setCrawlStorageFolder(crawlStorageFolder);
+
+        /*
+         * Instantiate the controller for this crawl.
+         */
+        PageFetcher pageFetcher = new PageFetcher(config);
+        RobotstxtConfig robotstxtConfig = new RobotstxtConfig();
+        RobotstxtServer robotstxtServer = new RobotstxtServer(robotstxtConfig, pageFetcher);
+        CrawlController controller = null;
+        try {
+            controller = new CrawlController(config, pageFetcher, robotstxtServer);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        /*
+         * For each crawl, you need to add some seed urls. These are the first
+         * URLs that are fetched and then the crawler starts following links
+         * which are found in these pages
+         */
+        controller.addSeed("http://www.ics.uci.edu/~lopes/");
+        controller.addSeed("http://www.ics.uci.edu/~welling/");
+        controller.addSeed("http://www.ics.uci.edu/");
+
+        /*
+         * Start the crawl. This is a blocking operation, meaning that your code
+         * will reach the line after this only when crawling is finished.
+         */
+        controller.start(MyCrawler.class, numberOfCrawlers);
+    }
+}
