dcc63f7623a79d7b26edb68fcd13c1a1bc7fbaa1
fuqin <shenghanming@gmail.com>
2016-07-24 22:56:54 +0800

    add Bluetooth Loe Energy

---
+++ app/src/main/java/com/hansheng/studynote/bluetooth/BluetoothLE/SampleGattAttributes.java
@@ -0,0 +1,27 @@
+package com.hansheng.studynote.bluetooth.BluetoothLE;
+
+import java.util.HashMap;
+
+/**
+ * Created by hansheng on 2016/7/24.
+ */
+public class SampleGattAttributes {
+
+    private static HashMap<String, String> attributes = new HashMap();
+    public static String HEART_RATE_MEASUREMENT = "00002a37-0000-1000-8000-00805f9b34fb";
+    public static String CLIENT_CHARACTERISTIC_CONFIG = "00002902-0000-1000-8000-00805f9b34fb";
+
+    static {
+        // Sample Services.
+        attributes.put("0000180d-0000-1000-8000-00805f9b34fb", "Heart Rate Service");
+        attributes.put("0000180a-0000-1000-8000-00805f9b34fb", "Device Information Service");
+        // Sample Characteristics.
+        attributes.put(HEART_RATE_MEASUREMENT, "Heart Rate Measurement");
+        attributes.put("00002a29-0000-1000-8000-00805f9b34fb", "Manufacturer Name String");
+    }
+
+    public static String lookup(String uuid, String defaultName) {
+        String name = attributes.get(uuid);
+        return name == null ? defaultName : name;
+    }
+}
