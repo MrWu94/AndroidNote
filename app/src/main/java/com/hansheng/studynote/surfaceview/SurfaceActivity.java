763d0f7c80387d1b2d591c89712c3d2558007c31
fuqin <shenghanming@gmail.com>
2016-08-01 22:21:47 +0800

    add surfaceview first use

---
+++ app/src/main/java/com/hansheng/studynote/surfaceview/SurfaceActivity.java
@@ -0,0 +1,18 @@
+package com.hansheng.studynote.surfaceview;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.hansheng.studynote.R;
+
+/**
+ * Created by hansheng on 2016/8/1.
+ */
+public class SurfaceActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.surface_layout);
+    }
+}
