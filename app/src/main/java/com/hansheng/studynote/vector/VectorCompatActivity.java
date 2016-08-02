e2634f4d66bdea01e80c79bb449f6473f68ebbd7
fuqin <shenghanming@gmail.com>
2016-08-02 21:05:25 +0800

    add vector

---
+++ app/src/main/java/com/hansheng/studynote/vector/VectorCompatActivity.java
@@ -0,0 +1,65 @@
+package com.hansheng.studynote.vector;
+
+import android.content.Intent;
+import android.graphics.drawable.Animatable;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatDelegate;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.hansheng.studynote.R;
+
+/**
+ * Created by hansheng on 2016/8/2.
+ */
+public class VectorCompatActivity extends AppCompatActivity {
+
+
+    static {
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pre_l);
+    }
+    public void anim(View view) {
+        ImageView imageView = (ImageView) view;
+        Drawable drawable = imageView.getDrawable();
+        if (drawable instanceof Animatable) {
+            ((Animatable) drawable).start();
+        }
+    }
+
+    private void switchToL() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            startActivity(new Intent(VectorCompatActivity.this, LActivity.class));
+        } else {
+            Toast.makeText(VectorCompatActivity.this, "系统版本不支持L plus", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            switchToL();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
