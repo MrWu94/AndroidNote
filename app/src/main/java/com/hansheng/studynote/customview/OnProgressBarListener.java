7b66b7f0e0ac0d6c74a69dd3f76ca45dda5af804
fuqin <shenghanming@gmail.com>
2016-09-23 17:47:14 +0800

    add  getHeight getWidth

---
+++ app/src/main/java/com/hansheng/studynote/customview/OnProgressBarListener.java
@@ -0,0 +1,9 @@
+package com.hansheng.studynote.customview;
+
+/**
+ * Created by hansheng on 16-9-23.
+ */
+
+public interface OnProgressBarListener {
+    void onProgressChange(int current, int max);
+}
--- app/src/main/java/com/hansheng/studynote/customview/PaddingActivity.java
+++ app/src/main/java/com/hansheng/studynote/customview/PaddingActivity.java
@@ -6,17 +6,35 @@ import android.support.v7.app.AppCompatActivity;
 
 import com.hansheng.studynote.R;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 /**
  * Created by hansheng on 16-9-13.
  */
 
 public class PaddingActivity extends AppCompatActivity{
     private ArcProgress arcProgress;
+    private NumberProgressBar numberProgressBar;
+    private Timer timer;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.padding_layout);
         arcProgress= (ArcProgress) findViewById(R.id.arc_process);
+        numberProgressBar= (NumberProgressBar) findViewById(R.id.numberbar1);
         arcProgress.setProgress(50);
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        numberProgressBar.incrementProgressBy(1);
+                    }
+                });
+            }
+        }, 1000, 100);
     }
 }
