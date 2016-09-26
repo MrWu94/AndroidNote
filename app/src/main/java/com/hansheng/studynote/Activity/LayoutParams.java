d722a38fb4229a388ecb361d96baf7c957f3ae35
shm <shenghanming@gmail.com>
2016-09-26 15:29:32 +0800

    Merge branch 'master' of /home/hansheng/AndroidStudioProjects/StudyNote with conflicts.

---
+++ app/src/main/java/com/hansheng/studynote/Activity/LayoutParams.java
@@ -0,0 +1,8 @@
+package com.hansheng.studynote.Activity;
+
+/**
+ * Created by hansheng on 16-9-24.
+ */
+
+public class LayoutParams {
+}
--- app/src/main/java/com/hansheng/studynote/customview/PaddingActivity.java
+++ app/src/main/java/com/hansheng/studynote/customview/PaddingActivity.java
@@ -1,8 +1,11 @@
 package com.hansheng.studynote.customview;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 
 import com.hansheng.studynote.R;
 
@@ -14,15 +17,18 @@ import java.util.TimerTask;
  */
 
 public class PaddingActivity extends AppCompatActivity{
+    private final static String TAG=PaddingActivity.class.getSimpleName();
     private ArcProgress arcProgress;
     private NumberProgressBar numberProgressBar;
     private Timer timer;
+    private AnimDownloadProgressButton mAnimDownloadProgressButton;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.padding_layout);
         arcProgress= (ArcProgress) findViewById(R.id.arc_process);
         numberProgressBar= (NumberProgressBar) findViewById(R.id.numberbar1);
+        mAnimDownloadProgressButton= (AnimDownloadProgressButton) findViewById(R.id.anim_btn);
         arcProgress.setProgress(50);
         timer = new Timer();
         timer.schedule(new TimerTask() {
@@ -36,5 +42,29 @@ public class PaddingActivity extends AppCompatActivity{
                 });
             }
         }, 1000, 100);
+
+        mAnimDownloadProgressButton.setCurrentText("安装");
+        mAnimDownloadProgressButton.setTextSize(60f);
+        mAnimDownloadProgressButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showTheButton();
+            }
+        });
+    }
+    private void showTheButton() {
+        mAnimDownloadProgressButton.setState(AnimDownloadProgressButton.DOWNLOADING);
+        mAnimDownloadProgressButton.setProgressText("下载中", mAnimDownloadProgressButton.getProgress() + 8);
+        Log.d(TAG, "showTheButton: " + mAnimDownloadProgressButton.getProgress());
+        if (mAnimDownloadProgressButton.getProgress() + 10 > 100) {
+            mAnimDownloadProgressButton.setState(AnimDownloadProgressButton.INSTALLING);
+            mAnimDownloadProgressButton.setCurrentText("安装中");
+            new Handler().postDelayed(new Runnable() {
+                public void run() {
+                    mAnimDownloadProgressButton.setState(AnimDownloadProgressButton.NORMAL);
+                    mAnimDownloadProgressButton.setCurrentText("打开");
+                }
+            }, 2000);   //2秒
+        }
     }
 }
