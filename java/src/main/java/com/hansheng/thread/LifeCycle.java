d0cb19abb0cde8a30411ef98accc1fa3754baaca
fuqin <shenghanming@gmail.com>
2016-10-04 00:35:55 +0800

    add Spinner and Transition

---
+++ java/src/main/java/com/hansheng/thread/LifeCycle.java
@@ -0,0 +1,36 @@
+package com.hansheng.thread;
+
+/**
+ * Created by hansheng on 2016/10/3.
+ * join()：等待该线程终止。
+ * ，在上面的代码中使用了join方法，这个方法的主要功能是保证线程的run方法完成后程序才继续运行
+ *  一但线程开始执行run方法，就会一直到这个run方法执行完成这个线程才退出。但在线程执行的过程中
+ *  可以通过两个方法使线程暂时停止执行。这两个方法是yield和sleep。thread.yield()在多线程程序中,
+ *  为了防止某线程独占CPU资源(这样其它的线程就得不到"响应"了).可以让当前执行的线程"休息"一下.
+ *  但是这种thread.yield() 调用,并不保证下一个运行的线程就一定不是该线程.而使用sleep使线程休眠后，
+ *  只能在设定的时间后使线程处于就绪状态（在线程休眠结束后，线程不一定会马上执行，只是进入了就绪状态，
+ *  等待着系统进行调度）。在使用sleep时要注意，不能在一个线程中来休眠另一个线程。如main方法中使用
+ *  thread.sleep(2000)方法是无法使thread线程休眠2秒的，而只能使主线程休眠2秒。在使用sleep方法
+ *  时有四点需要注意：
+ *   sleep()可使优先级低的线程得到执行的机会，当然也可以让同优先级和高优先级的线程有执行的机会；
+ *   yield()只能使同优先级的线程有执行的机会。
+ */
+public class LifeCycle extends Thread
+{
+    public void run()
+    {
+        int n = 0;
+        while ((++n) < 1000);
+    }
+
+    public static void main(String[] args) throws Exception
+    {
+        LifeCycle thread1 = new LifeCycle();
+        System.out.println("isAlive: " + thread1.isAlive());
+        thread1.start();
+        System.out.println("isAlive: " + thread1.isAlive());
+        thread1.join();  // 等线程thread1结束后再继续执行
+        System.out.println("thread1已经结束!");
+        System.out.println("isAlive: " + thread1.isAlive());
+    }
+}
---
+++ java/src/main/java/com/hansheng/thread/TextThreadMethod.java
@@ -0,0 +1,38 @@
+package com.hansheng.thread;
+
+/**
+ * Created by hansheng on 2016/10/3.
+ * 调用yield()，不同优先级的线程永远不会得到执行机会。
+ * sleep()使当前线程进入停滞状态，所以执行sleep()的线程在指定的时间内肯定不会执行；
+ * yield()只是使当前线程重新回到可执行状态，所以执行yield()的线程有可能在进入到可执行状态后马上又被执行。
+ * yield()：暂停当前正在执行的线程对象，并执行其他线程。
+ */
+class TestThreadMethod extends Thread{
+    public static int shareVar = 0;
+    public TestThreadMethod(String name){
+        super(name);
+    }
+    public void run(){
+        for(int i=0; i<4; i++){
+            System.out.print(Thread.currentThread().getName());
+            System.out.println(" : " + i);
+          Thread.yield();
+//          /* （2） */
+//            try{
+//                Thread.sleep(3000);
+//            }
+//            catch(InterruptedException e){
+//                System.out.println("Interrupted");
+//            }}
+            }}
+}
+ class TestThread{
+    public static void main(String[] args){
+        TestThreadMethod t1 = new TestThreadMethod("t1");
+        TestThreadMethod t2 = new TestThreadMethod("t2");
+        t1.setPriority(Thread.MAX_PRIORITY);
+        t2.setPriority(Thread.MIN_PRIORITY);
+        t1.start();
+        t2.start();
+    }
+}
