32b1d1664d698b5ca04c1c009a52dffa521d81f8
fuqin <shenghanming@gmail.com>
2016-11-07 17:05:53 +0800

    add  seetings all font

---
+++ app/src/main/java/com/hansheng/studynote/Activity/BaseActivity.java
@@ -0,0 +1,62 @@
+package com.hansheng.studynote.Activity;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.view.LayoutInflaterCompat;
+import android.support.v4.view.LayoutInflaterFactory;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatDelegate;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by hansheng on 16-11-7.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+    public static Typeface typeface;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+//
+//        if (typeface == null)
+//        {
+//            typeface = Typeface.createFromAsset(getAssets(), "Ruthie.ttf");
+//        }
+//        LayoutInflaterCompat.setFactory(LayoutInflater.from(this), new LayoutInflaterFactory()
+//        {
+//            @Override
+//            public View onCreateView(View parent, String name, Context context, AttributeSet attrs)
+//            {
+//                AppCompatDelegate delegate = getDelegate();
+//                View view = delegate.createView(parent, name, context, attrs);
+//
+//                if ( view!= null && (view instanceof TextView))
+//                {
+//                    ((TextView) view).setTypeface(typeface);
+//                }
+//                return view;
+//            }
+//        });
+        super.onCreate(savedInstanceState);
+    }
+
+    public void setTypeface(ViewGroup root, Typeface typeface){
+        if(root==null || typeface==null){
+            return;
+        }
+        int count = root.getChildCount();
+        for(int i=0;i<count;++i){
+            View view = root.getChildAt(i);
+            if(view instanceof TextView){
+                ((TextView)view).setTypeface(typeface);
+            }else if(view instanceof ViewGroup){
+                setTypeface((ViewGroup)view, typeface);
+            }
+        }
+    }
+}
--- app/src/main/java/com/hansheng/studynote/Activity/CustomFontActivity.java
+++ app/src/main/java/com/hansheng/studynote/Activity/CustomFontActivity.java
@@ -4,6 +4,8 @@ import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutCompat;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.hansheng.studynote.R;
@@ -15,16 +17,22 @@ import org.w3c.dom.Text;
  * Created by hansheng on 16-10-9.
  */
 
-public class CustomFontActivity extends AppCompatActivity{
+public class CustomFontActivity extends BaseActivity{
     Typeface typeface;
     private TextView textView;
+    private LinearLayout linearLayout;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         typeface=Typeface.createFromAsset(getAssets(),"Ruthie.ttf");
         setContentView(R.layout.custom_main);
         textView= (TextView) findViewById(R.id.text);
-        textView.setTypeface(typeface);
+        linearLayout= (LinearLayout) findViewById(R.id.linear);
+        setTypeface(linearLayout,typeface);
+
+
+//        textView.setTypeface(typeface);
 
 
     }
