92eb5c55921579f8d5b38dc6cf2502ad5e54242c
shm <shenghanming@gmail.com>
2016-11-26 17:50:24 +0800

    add https

---
+++ app/src/main/java/com/hansheng/studynote/Http/HttpClient/HttpGetActivity.java
@@ -0,0 +1,65 @@
+package com.hansheng.studynote.Http.HttpClient;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.hansheng.studynote.R;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.InputStream;
+
+/**
+ * Created by hansheng on 16-11-26.
+ */
+public class HttpGetActivity extends Activity {
+    String uri = "https://www.baidu.com/";
+    final String TAG_STRING = "TAG";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.item_list);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    //得到HttpClient对象
+                    HttpClient getClient = new DefaultHttpClient();
+                    //得到HttpGet对象
+                    HttpGet request = new HttpGet(uri);
+                    //客户端使用GET方式执行请教，获得服务器端的回应response
+                    HttpResponse response = getClient.execute(request);
+                    System.out.println("======");
+                    //判断请求是否成功
+                    if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+                        Log.i(TAG_STRING, "请求服务器端成功");
+                        //获得输入流
+                        InputStream inStrem = response.getEntity().getContent();
+                        int result = inStrem.read();
+                        while (result != -1) {
+                            System.out.print((char) result);
+                            result = inStrem.read();
+                        }
+                        //关闭输入流
+                        inStrem.close();
+                    } else {
+                        Log.i(TAG_STRING, "请求服务器端失败");
+                    }
+                } catch (Exception e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+
+    }
+}
+
---
+++ app/src/main/java/com/hansheng/studynote/Http/HttpUrlConnection/HttpUrlConnectionActivity.java
@@ -0,0 +1,62 @@
+package com.hansheng.studynote.Http.HttpUrlConnection;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.hansheng.studynote.R;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by hansheng on 16-11-26.
+ */
+
+public class HttpUrlConnectionActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.item_list);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    // 请求地址
+                    String parm = "https://www.baidu.com/";
+                    // 创建url
+                    URL url = new URL(parm);
+                    // 使用httpurlconnection 进行请求的发送
+                    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                    // 设置相关属性,如果是post请求 请求参数在正文中所以允许输出流，如果是get        请求不用输出了因为参数是在url之后
+                    con.setDoOutput(false);
+                    // 响应数据的接受，所以我们要允许输入流
+                    con.setDoInput(true);
+                    // 设置是否要缓存
+                    con.setUseCaches(false);
+                    //Java对象的序列化，进行流的传递
+                    //con.setRequestProperty(URLEncoder.encode("Content-type","UTF-8"),URLEncoder.encode("application/x-java-serialized-object","UTF-8"));
+                    // 设置请求方式
+                    con.setRequestMethod("GET");
+                    //上面这些都是请求的一些属性的配置，也就是规则的定制
+                    //建立一个tcp连接
+                    con.connect();
+                    // 接收请求响应数据
+                    InputStream inStrm = con.getInputStream();
+                    byte[] buffer = new byte[1024];
+                    inStrm.read(buffer);
+                    // 编码设置
+                    String body = new String(buffer, "UTF-8");
+                    System.out.println("返回内容" + body.toString());
+                } catch (Exception e) {
+
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+
+    }
+}
