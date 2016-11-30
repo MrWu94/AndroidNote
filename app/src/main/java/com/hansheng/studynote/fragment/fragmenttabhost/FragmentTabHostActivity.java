8e1e5cbb9f3d8454e45d1de6e2e6f269afb7c0e7
shm <shenghanming@gmail.com>
2016-11-30 17:41:19 +0800

    add fragmentabhost

---
+++ app/src/main/java/com/hansheng/studynote/fragment/fragmenttabhost/FragmentTabHostActivity.java
@@ -0,0 +1,71 @@
+package com.hansheng.studynote.fragment.fragmenttabhost;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.support.annotation.DrawableRes;
+import android.support.v4.app.FragmentTabHost;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TabHost;
+
+import com.hansheng.studynote.R;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+/**
+ * Created by hansheng on 16-11-30.
+ */
+
+public class FragmentTabHostActivity extends AppCompatActivity {
+
+    @Bind(android.R.id.tabhost)
+    FragmentTabHost mTabHost;
+
+    // 图片
+    @DrawableRes
+    private int mImages[] = {
+            R.drawable.tab_counter,
+            R.drawable.tab_assistant,
+            R.drawable.tab_contest,
+            R.drawable.tab_center
+    };
+
+    // 标题
+    private String mFragmentTags[] = {
+            "counter",
+            "assistant",
+            "contest",
+            "center"
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_main);
+
+        ButterKnife.bind(this);
+
+        mTabHost.setup(this, getSupportFragmentManager(), android.R.id.tabcontent);
+        mTabHost.getTabWidget().setDividerDrawable(null); // 去掉分割线
+
+        for (int i = 0; i < mImages.length; i++) {
+            // Tab按钮添加文字和图片
+            TabHost.TabSpec tabSpec = mTabHost.newTabSpec(mFragmentTags[i]).setIndicator(getImageView(i));
+            // 添加Fragment
+            mTabHost.addTab(tabSpec, FragmentTab.class, null);
+            // 设置Tab按钮的背景
+            mTabHost.getTabWidget().getChildAt(i).setBackgroundResource(R.color.pedo_actionbar_bkg);
+        }
+    }
+
+    // 获得图片资源
+    private View getImageView(int index) {
+        @SuppressLint("InflateParams")
+        View view = getLayoutInflater().inflate(R.layout.view_tab_indicator, null);
+        ImageView imageView = (ImageView) view.findViewById(R.id.tab_iv_image);
+        imageView.setImageResource(mImages[index]);
+        return view;
+    }
+}
