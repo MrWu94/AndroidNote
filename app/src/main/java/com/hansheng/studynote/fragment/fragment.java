d3349d35493d4ece7c533c2814cd88c859b1f829
shm <shenghanming@gmail.com>
2016-12-01 17:06:18 +0800

    add fragment

--- app/src/main/java/com/hansheng/studynote/fragment/fragment.java
+++ app/src/main/java/com/hansheng/studynote/fragment/fragment.java
@@ -11,9 +11,29 @@ import com.hansheng.studynote.R;
 
 /**
  * Created by hansheng on 16-9-9.
+ * 用法非常简单，直接在Fragment类中重写上述方法即可。
+ * 当isVisibleToUser=true的时候，执行相应的操作，例如：开辟线程和后台服务器进行交互获取数据，
+ * 进行界面数据加载显示等，或者进行数据统计，统计当前Fragment打开过几次等等。
+ * 当isVisibleToUser= false的时候，说明当前Fragment不可见，大家可以根据需求执行响应的操作。
  */
 
-public class fragment  extends Fragment{
+public class fragment extends Fragment {
+
+    @Override
+    public void onHiddenChanged(boolean hidden) {
+        super.onHiddenChanged(hidden);
+    }
+
+    @Override
+    public void setUserVisibleHint(boolean isVisibleToUser) {
+        super.setUserVisibleHint(isVisibleToUser);
+        if (isVisibleToUser) {
+            //可见时执行的操作
+        } else {
+            //不可见时执行的操作
+        }
+    }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +42,6 @@ public class fragment  extends Fragment{
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_activity,container,false);
+        return inflater.inflate(R.layout.fragment_activity, container, false);
     }
 }
--- app/src/main/java/com/hansheng/studynote/viewpager/fragmentpageradapter/MyFragmentPagerAdapter.java
+++ app/src/main/java/com/hansheng/studynote/viewpager/fragmentpageradapter/MyFragmentPagerAdapter.java
@@ -8,11 +8,17 @@ import java.util.List;
 
 /**
  * Created by wfq on 2016/11/30.
+ * FragmentPagerAdapter
+ * <p>
+ * 适合于 Fragment数量不多的情况。当某个页面不可见时，该页面对应的View可能会被销毁，但是所有的Fragment都会一直存在于内存中。
+ * 如果Fragment需要保存的状态较多时，
+ * 会导致占用内存较大，因此对于Fragment数量较多的情况，建议使用FragmentStatePagerAdapter。
  */
 
 public class MyFragmentPagerAdapter extends FragmentPagerAdapter {
     private List<Fragment> fragList;
     private List<String> titleList;
+
     public MyFragmentPagerAdapter(FragmentManager fm, List<Fragment> fragList, List<String> titleList) {
         super(fm);
         this.fragList = fragList;
