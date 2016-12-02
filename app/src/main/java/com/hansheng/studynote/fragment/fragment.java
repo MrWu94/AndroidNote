d2b5290b3631f836b66f7f6822aa0ac5bfa4271c
shm <shenghanming@gmail.com>
2016-12-02 17:14:37 +0800

    add fragment

--- app/src/main/java/com/hansheng/studynote/fragment/fragment.java
+++ app/src/main/java/com/hansheng/studynote/fragment/fragment.java
@@ -15,6 +15,13 @@ import com.hansheng.studynote.R;
  * 当isVisibleToUser=true的时候，执行相应的操作，例如：开辟线程和后台服务器进行交互获取数据，
  * 进行界面数据加载显示等，或者进行数据统计，统计当前Fragment打开过几次等等。
  * 当isVisibleToUser= false的时候，说明当前Fragment不可见，大家可以根据需求执行响应的操作。
+ * onAttach方法：Fragment和Activity建立关联的时候调用。
+ * onCreateView方法：为Fragment加载布局时调用。
+ * onActivityCreated方法：当Activity中的onCreate方法执行完后调用。
+ * onDestroyView方法：Fragment中的布局被移除时调用。
+ * onDetach方法：Fragment和Activity解除关联的时候调用。
+ *
+ * 记得replace是remove和add的合体，
  */
 
 public class fragment extends Fragment {
---
+++ app/src/main/java/com/hansheng/studynote/fragment/fragmentadd/FragmentMainActivity.java
@@ -0,0 +1,44 @@
+package com.hansheng.studynote.fragment.fragmentadd;
+
+import android.app.Activity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.view.Window;
+
+import com.hansheng.studynote.R;
+
+/**
+ * Created by hansheng on 16-12-2.
+ * 1、为什么需要判null呢？
+ * 主要是因为，当Activity因为配置发生改变（屏幕旋转）或者内存不足被系统杀死，造成重新创建时，我们的fragment会被保存下来，
+ * 但是会创建新的FragmentManager，
+ * 新的FragmentManager会首先会去获取保存下来的fragment队列，重建fragment队列，从而恢复之前的状态。
+ * <p>
+ * 2、add(R.id.id_fragment_container,mContentFragment)中的布局的id有何作用？
+ * 一方面呢，是告知FragmentManager，此fragment的位置；另一方面是此fragment的唯一标识；
+ * 就像我们上面通过fm.findFragmentById(R.id.id_fragment_container)查找~~
+ */
+
+public class FragmentMainActivity extends Activity {
+
+    private FragmentOne fragmentOne;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.frag_main);
+
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction tx = fm.beginTransaction();
+        fragmentOne = (FragmentOne) fm.findFragmentById(R.id.id_content);
+        if (fragmentOne == null) {
+            tx.add(R.id.id_content, new FragmentOne(), "ONE");
+            tx.commit();
+        }
+    }
+
+}
