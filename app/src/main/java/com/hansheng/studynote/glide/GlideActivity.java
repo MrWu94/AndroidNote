c56c7b890d8fb4de40b5dc4c4f41982e93e89e64
shm <shenghanming@gmail.com>
2016-12-28 15:02:24 +0800

    add glide

--- app/src/main/java/com/hansheng/studynote/glide/GlideActivity.java
+++ app/src/main/java/com/hansheng/studynote/glide/GlideActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.hansheng.studynote.R;
@@ -16,14 +15,33 @@ import com.hansheng.studynote.R;
 
 public class GlideActivity extends AppCompatActivity {
     private ImageView imageView;
+    private ImageView imageRec;
+    private ImageView imageGif;
+    private ImageView imageGifBitmap;
+    String gifUrl = "http://i.kinja-img.com/gawker-media/image/upload/s--B7tUiM5l--/gf2r69yorbdesguga10i.gif";
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.glide_layout);
-        imageView= (ImageView) findViewById(R.id.glide_iamgeview);
+        imageRec = (ImageView) findViewById(R.id.img_resource);
+        imageView = (ImageView) findViewById(R.id.glide_iamgeview);
+        imageGif = (ImageView) findViewById(R.id.img_gif);
+        imageGifBitmap = (ImageView) findViewById(R.id.img_gifbitmap);
         Glide.with(this).load("https://avatars0.githubusercontent.com/u/12311938?v=3&s=40")
                 .placeholder(R.drawable.ic_launcher)
                 .transform(new GlideCircleTransform(this))
                 .into(imageView);
+
+        int resourceId = R.drawable.ic_launcher;
+
+        Glide.with(this).load(resourceId).into(imageRec);
+        Glide.with(this).load(gifUrl)
+                .into(imageGif);
+        Glide.with(this).load(gifUrl)
+                .asBitmap()
+                .into(imageGifBitmap);
+
+
     }
 }
