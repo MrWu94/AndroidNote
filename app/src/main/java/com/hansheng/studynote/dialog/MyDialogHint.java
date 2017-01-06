34b3c54e9788f75f1c8afc6b498fe94ad4701194
shm <shenghanming@gmail.com>
2017-01-06 14:06:24 +0800

    modify code style

--- app/src/main/java/com/hansheng/studynote/dialog/MyDialogHint.java
+++ app/src/main/java/com/hansheng/studynote/dialog/MyDialogHint.java
@@ -8,14 +8,19 @@ import android.widget.TextView;
 
 import com.hansheng.studynote.R;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 /**
  * Created by hansheng on 16-9-26.
  */
 
 public class MyDialogHint extends Dialog implements View.OnClickListener {
-    private TextView dialog_hint_text_yes;
-    private TextView dialoh_hint_text_no;
 
+    @Bind(R.id.dialog_hint_text_yes)
+    TextView dialog_hint_text_yes;
+    @Bind(R.id.dialoh_hint_text_no)
+    TextView dialoh_hint_text_no;
 
 
     public MyDialogHint(Context context) {
@@ -27,20 +32,21 @@ public class MyDialogHint extends Dialog implements View.OnClickListener {
 
 
     }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.dialog_hint);
+        ButterKnife.bind(this);
         initview();
     }
 
 
     private void initview() {
-        dialog_hint_text_yes = (TextView) findViewById(R.id.dialog_hint_text_yes);
-        dialoh_hint_text_no = (TextView) findViewById(R.id.dialoh_hint_text_no);
         dialoh_hint_text_no.setOnClickListener(this);
         dialog_hint_text_yes.setOnClickListener(this);
     }
+
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
@@ -48,7 +54,6 @@ public class MyDialogHint extends Dialog implements View.OnClickListener {
                 dismiss();
                 break;
             case R.id.dialog_hint_text_yes:
-
                 dismiss();
                 break;
         }
