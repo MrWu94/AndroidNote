ebad0fe54af9aaabf92fae638d630beb8fe5fdfb
shm <shenghanming@gmail.com>
2017-01-06 17:36:17 +0800

    add customview orcode view

---
+++ app/src/main/java/com/hansheng/studynote/customview/ScanBoxActivity.java
@@ -0,0 +1,26 @@
+package com.hansheng.studynote.customview;
+
+import com.hansheng.studynote.Activity.BaseActivity;
+import com.hansheng.studynote.R;
+
+/**
+ * Created by hansheng on 17-1-6.
+ */
+
+public class ScanBoxActivity extends BaseActivity {
+
+    protected ScanBoxView mScanBoxView;
+
+
+
+
+    @Override
+    protected int initContentView() {
+        return R.layout.scanbox_layout;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+}
