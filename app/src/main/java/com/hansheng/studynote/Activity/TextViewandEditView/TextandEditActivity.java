2d452a4e9ed9218ab847b65f32a83d04106a76a3
shm <shenghanming@gmail.com>
2017-01-20 14:01:20 +0800

    add Base

---
+++ app/src/main/java/com/hansheng/studynote/Activity/TextViewandEditView/TextandEditActivity.java
@@ -0,0 +1,20 @@
+package com.hansheng.studynote.Activity.TextViewandEditView;
+
+import com.hansheng.studynote.Activity.BaseActivity;
+import com.hansheng.studynote.R;
+
+/**
+ * Created by hansheng on 17-1-20.
+ */
+
+public class TextandEditActivity extends BaseActivity{
+    @Override
+    protected int initContentView() {
+        return R.layout.text_edit_main;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+}
