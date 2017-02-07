c9d83ec67bee905edebebddb941f87aa0504dd43
shm <shenghanming@gmail.com>
2017-02-07 16:13:40 +0800

    add OnPageChangeListener

--- app/src/main/java/com/hansheng/studynote/viewpager/MusicViewPager/MusicActivity.java
+++ app/src/main/java/com/hansheng/studynote/viewpager/MusicViewPager/MusicActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatDelegate;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.widget.RadioButton;
 
 import com.hansheng.studynote.R;
@@ -19,10 +20,18 @@ import butterknife.OnCheckedChanged;
 
 /**
  * Created by hansheng on 16-9-21.
+ * onPageScrollStateChanged(int arg0) ，此方法是在状态改变的时候调用，其中arg0这个参数有三种状态（0，1，2）。arg0 ==1表示正在滑动，
+ * arg0==2表示滑动完毕了，arg0==0表示什么都没做。当页面开始滑动的时候，三种状态的变化顺序为（1，2，0）。
+ *
+ * onPageScrolled(int arg0,float arg1,int arg2) ，当页面在滑动的时候会调用此方法，在滑动被停止之前，此方法回一直得到调用。
+ * 其中三个参数的含义分别为：arg0 :当前页面，及你点击滑动的页面。arg1:当前页面偏移的百分比。arg2:当前页面偏移的像素位置。
+ *
+ * onPageSelected(int arg0) ，此方法是页面跳转完后得到调用，arg0是你当前选中的页面的position。
  */
 
 public class MusicActivity extends BActivity {
     static final int DEFAULT_PAGE_INDEX = 2;
+    private static final String TAG="MusicActivity";
 
     @Bind(R.id.toolbar)
     Toolbar toolbar;
@@ -63,16 +72,23 @@ public class MusicActivity extends BActivity {
         viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
             @Override
             public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                Log.d(TAG, "onPageScrolled: position="+position+"  positionOffset="+positionOffset+" positionOffsetPixels="+positionOffsetPixels);
                 // Empty
+//                viewPager.getChildAt(position);
+//                viewPager.getCurrentItem();
+
+
             }
 
             @Override
             public void onPageScrollStateChanged(int state) {
+                Log.d(TAG, "onPageScrollStateChanged: state"+state);
                 // Empty
             }
 
             @Override
             public void onPageSelected(int position) {
+                Log.d(TAG, "onPageSelected: position"+position);
                 radioButtons.get(position).setChecked(true);
             }
         });
