db9e4aa3d93c8f6af1d623dd18b71426a86297de
fuqin <shenghanming@gmail.com>
2017-02-22 16:41:39 +0800

    add Interview

---
+++ java/src/main/java/com/hansheng/Interview/ListNodeDemo.java
@@ -0,0 +1,14 @@
+package com.hansheng.Interview;
+
+/**
+ * Created by hansheng on 17-2-22.
+ */
+
+public class ListNodeDemo {
+    public static void main(String... args){
+    }
+
+    public static class ListNode{
+        
+    }
+}
