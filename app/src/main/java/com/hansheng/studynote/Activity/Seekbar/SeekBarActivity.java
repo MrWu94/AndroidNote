18733cbf9f8809665aaabc67242132263c009a6c
shm <shenghanming@gmail.com>
2017-02-28 14:54:26 +0800

    add seekbar and Interview

---
+++ app/src/main/java/com/hansheng/studynote/Activity/Seekbar/SeekBarActivity.java
@@ -0,0 +1,19 @@
+package com.hansheng.studynote.Activity.Seekbar;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.hansheng.studynote.R;
+
+/**
+ * Created by hansheng on 17-2-27.
+ */
+
+public class SeekBarActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.seekbar_main);
+    }
+}
