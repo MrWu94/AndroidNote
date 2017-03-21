73b1f7baebfb5d490059caf580caf7a9a7f2c69a
fuqin <shenghanming@gmail.com>
2017-03-21 23:39:27 +0800

    commit final

---
+++ java/src/main/java/com/hansheng/String/StringEquel.java
@@ -0,0 +1,17 @@
+package com.hansheng.String;
+
+/**
+ * Created by mrwu on 2017/3/21.
+ */
+
+public class StringEquel {
+     public static void main(String... args){
+         String a="dp";
+         String b="dp";
+         String x="d";
+         String y="p";
+         String c=x+y;
+         System.out.println(a==b);
+         System.out.println(a==c);
+     }
+}
