5ce05f4a1fb7499a7e3c10f1cb5dd9c1ba67389b
shm <shenghanming@gmail.com>
2017-04-05 19:18:31 +0800

    commit wifi experience

--- app/src/main/java/com/hansheng/studynote/wifi/wifimanager/WifiManagerActivity.java
+++ app/src/main/java/com/hansheng/studynote/wifi/wifimanager/WifiManagerActivity.java
@@ -3,6 +3,7 @@ package com.hansheng.studynote.wifi.wifimanager;
 
 import android.net.DhcpInfo;
 import android.net.wifi.ScanResult;
+import android.net.wifi.WifiConfiguration;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
@@ -23,14 +24,18 @@ public class WifiManagerActivity extends AppCompatActivity {
     public static final String TAG="WifiManagerActivity";
     public static final String NO_PASSWORD = "[ESS]";
     public static final String NO_PASSWORD_WPS = "[WPS][ESS]";
+    public static final int WIFICIPHER_NOPASS = 1;
+    public static final int WIFICIPHER_WEP = 2;
+    public static final int WIFICIPHER_WPA = 3;
     public WifiInfo wifiInfo;
     public DhcpInfo dhcpInfo;
+    public WifiManager wifiManager;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.acceler_layout);
-        WifiManager wifiManager= (WifiManager) getSystemService(WIFI_SERVICE);
+         wifiManager= (WifiManager) getSystemService(WIFI_SERVICE);
 //        wifiManager.setWifiEnabled(true);
         wifiInfo = wifiManager.getConnectionInfo();
         dhcpInfo = wifiManager.getDhcpInfo();
@@ -47,6 +52,7 @@ public class WifiManagerActivity extends AppCompatActivity {
         for (ScanResult scanResult : scanResult1) {
             Log.d(TAG, "onCreate: "+"\n设备名："+scanResult.SSID
                     +" 信号强度："+scanResult.level+"/n :"+wifiManager.calculateSignalLevel(scanResult.level,4));
+            addNetwork(createWifiCfg(scanResult.SSID, null,1));
         }
     }
     /**
@@ -70,6 +76,92 @@ public class WifiManagerActivity extends AppCompatActivity {
         return resultList;
     }
 
+    /**
+     * 添加到指定Wifi网络 /切换到指定Wifi网络
+     * @param wf
+     * @return
+     */
+    public boolean addNetwork(WifiConfiguration wf){
+        //断开当前的连接
+        disconnectCurrentNetwork();
+
+        //连接新的连接
+        int netId = wifiManager.addNetwork(wf);
+        boolean enable = wifiManager.enableNetwork(netId, true);
+        return enable;
+    }
+
+    /**
+     * 关闭当前的Wifi网络
+     * @return
+     */
+    public boolean disconnectCurrentNetwork(){
+        if(wifiManager != null && wifiManager.isWifiEnabled()){
+            int netId = wifiManager.getConnectionInfo().getNetworkId();
+            wifiManager.disableNetwork(netId);
+            return wifiManager.disconnect();
+        }
+        return false;
+    }
+
+
+
+    /**
+     * 创建WifiConfiguration
+     *
+     * @param ssid
+     * @param password
+     * @param type
+     * @return
+     */
+    public static WifiConfiguration createWifiCfg(String ssid, String password, int type){
+        WifiConfiguration config = new WifiConfiguration();
+        config.allowedAuthAlgorithms.clear();
+        config.allowedGroupCiphers.clear();
+        config.allowedKeyManagement.clear();
+        config.allowedPairwiseCiphers.clear();
+        config.allowedProtocols.clear();
+
+        config.SSID = "\"" + ssid + "\"";
+
+        if(type == WIFICIPHER_NOPASS){
+//            config.wepKeys[0] = "";
+            config.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.NONE);
+//            config.wepTxKeyIndex = 0;
+
+//            无密码连接WIFI时，连接不上wifi，需要注释两行代码
+//            config.wepKeys[0] = "";
+//            config.wepTxKeyIndex = 0;
+        }else if(type == WIFICIPHER_WEP){
+            config.hiddenSSID = true;
+            config.wepKeys[0]= "\""+password+"\"";
+            config.allowedAuthAlgorithms.set(WifiConfiguration.AuthAlgorithm.SHARED);
+            config.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.CCMP);
+            config.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.TKIP);
+            config.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.WEP40);
+            config.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.WEP104);
+            config.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.NONE);
+            config.wepTxKeyIndex = 0;
+        }else if(type == WIFICIPHER_WPA){
+            config.preSharedKey = "\""+password+"\"";
+            config.hiddenSSID = true;
+            config.allowedAuthAlgorithms.set(WifiConfiguration.AuthAlgorithm.OPEN);
+            config.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.TKIP);
+            config.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.WPA_PSK);
+            config.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.TKIP);
+            config.allowedProtocols.set(WifiConfiguration.Protocol.WPA);
+            config.status = WifiConfiguration.Status.ENABLED;
+        }
+
+        return config;
+    }
+
+
+    /**
+     *
+     * @param value
+     * @return
+     */
     public String FormatString(int value){
         String strValue="";
         byte[] ary = intToByteArray(value);
