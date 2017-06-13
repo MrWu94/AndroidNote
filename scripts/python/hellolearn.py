cd035c31797a24a8e6f9524e85b3258a9fc1a336
shm <shenghanming@gmail.com>
2017-06-13 10:43:30 +0800

    commit python

--- scripts/python/hellolearn.py
+++ scripts/python/hellolearn.py
@@ -43,7 +43,7 @@ if birth < 2000:
 else:
     print '00后'
 
-d = {'hansheng': 1, 'dongzhi': 20}
+d={'hansheng': 1, 'dongzhi': 20}
 print d['hansheng']
 
 
@@ -52,3 +52,45 @@ def getCurrentTime(x):
         return x
     else:
         return -x
+
+
+print getCurrentTime(1)
+
+
+def power(x):
+    return x * x
+
+
+print power(2)
+
+
+def power(x, n=2):
+    s = 1
+    while n > 0:
+        n = n - 1
+        s = s * x
+    return s
+
+print 'power',power(5, 5)
+
+def calc(numbers):
+    sum = 0
+    for n in numbers:
+        sum = sum + n * n
+    return sum
+
+print calc([1, 2, 3, 4])
+print calc((1, 2, 3, 4))
+
+def calc(*numbers):
+    sum = 0
+    for n in numbers:
+        sum = sum + n * n
+    return sum
+
+
+print calc(1, 2, 3, 4)
+print calc(1, 2, 3, 4)
+
+num=[1,2,3]
+print calc(*num)
