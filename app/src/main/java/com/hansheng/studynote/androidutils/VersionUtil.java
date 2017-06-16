98322f38af252b6d8d9de15427d732d74559558c
shm <shenghanming@gmail.com>
2017-06-16 14:13:31 +0800

    commit version

---
+++ app/src/main/java/com/hansheng/studynote/androidutils/VersionUtil.java
@@ -0,0 +1,88 @@
+package com.hansheng.studynote.androidutils;
+
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.text.TextUtils;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+/**
+ * Created by hansheng on 17-6-16.
+ */
+
+public class VersionUtil {
+    public static String getKernelVersion() {
+        String kernelVersion = "";
+        InputStream inputStream = null;
+        try {
+            inputStream = new FileInputStream("/proc/version");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return kernelVersion;
+        }
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream), 8 * 1024);
+        String info = "";
+        String line = "";
+        try {
+            while ((line = bufferedReader.readLine()) != null) {
+                info += line;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                bufferedReader.close();
+                inputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        try {
+            if (info != "") {
+                final String keyword = "version ";
+                int index = info.indexOf(keyword);
+                line = info.substring(index + keyword.length());
+                index = line.indexOf(" ");
+                kernelVersion = line.substring(0, index);
+            }
+        } catch (IndexOutOfBoundsException e) {
+            e.printStackTrace();
+        }
+
+        return kernelVersion;
+    }
+
+    public static String getHandSetInfo(Context context) {
+        String handSetInfo =
+                "手机型号:" + Build.MODEL +
+                        ",SDK版本:" + Build.VERSION.SDK +
+                        ",系统版本:" + Build.VERSION.RELEASE +
+                        ",软件版本:" + getAppVersionName(context);
+        return handSetInfo;
+
+    }
+
+    //获取当前版本号
+    public static String getAppVersionName(Context context) {
+        String versionName = "";
+        try {
+            PackageManager packageManager = context.getPackageManager();
+            PackageInfo packageInfo = packageManager.getPackageInfo("com.hansheng.studynote", 0);
+            versionName = packageInfo.versionName;
+            if (TextUtils.isEmpty(versionName)) {
+                return "";
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return versionName;
+    }
+}
--- app/src/main/java/com/hansheng/studynote/ui/service/music/MusicActivity.java
+++ app/src/main/java/com/hansheng/studynote/ui/service/music/MusicActivity.java
@@ -1,28 +1,17 @@
 package com.hansheng.studynote.ui.service.music;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
-import android.os.Build;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
 import com.hansheng.studynote.R;
-
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import com.hansheng.studynote.androidutils.VersionUtil;
 
 /**
  * Created by hansheng on 17-6-13.
@@ -44,8 +33,8 @@ public class MusicActivity extends Activity implements Button.OnClickListener {
         image = (ImageView) findViewById(R.id.btn_img);
         btnStart.setOnClickListener(this);
         btnStop.setOnClickListener(this);
-        Log.d(TAG, "onCreate: " + getKernelVersion());
-        Log.d(TAG, "onCreate: "+getHandSetInfo());
+        Log.d(TAG, "onCreate: " + VersionUtil.getKernelVersion());
+        Log.d(TAG, "onCreate: " + VersionUtil.getHandSetInfo(this));
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
@@ -77,70 +66,5 @@ public class MusicActivity extends Activity implements Button.OnClickListener {
         }
     }
 
-    public static String getKernelVersion() {
-        String kernelVersion = "";
-        InputStream inputStream = null;
-        try {
-            inputStream = new FileInputStream("/proc/version");
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-            return kernelVersion;
-        }
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream), 8 * 1024);
-        String info = "";
-        String line = "";
-        try {
-            while ((line = bufferedReader.readLine()) != null) {
-                info += line;
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                bufferedReader.close();
-                inputStream.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
 
-        try {
-            if (info != "") {
-                final String keyword = "version ";
-                int index = info.indexOf(keyword);
-                line = info.substring(index + keyword.length());
-                index = line.indexOf(" ");
-                kernelVersion = line.substring(0, index);
-            }
-        } catch (IndexOutOfBoundsException e) {
-            e.printStackTrace();
-        }
-
-        return kernelVersion;
-    }
-
-    private String getHandSetInfo(){
-        String handSetInfo=
-                "手机型号:" + Build.MODEL +
-                        ",SDK版本:" + Build.VERSION.SDK +
-                        ",系统版本:" + Build.VERSION.RELEASE+
-                        ",软件版本:"+getAppVersionName(MusicActivity.this);
-        return handSetInfo;
-
-    }
-    //获取当前版本号
-    private  String getAppVersionName(Context context) {
-        String versionName = "";
-        try {
-            PackageManager packageManager = context.getPackageManager();
-            PackageInfo packageInfo = packageManager.getPackageInfo("com.hansheng.studynote", 0);
-            versionName = packageInfo.versionName;
-            if (TextUtils.isEmpty(versionName)) {
-                return "";
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return versionName;
-    }
 }
